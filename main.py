ff020d9075410d58f968a02d59585755f1f614c9
kadusic1 <adi.kadusic.22@size.ba>
2026-03-01 23:05:38 +0100

    fix: replaced load_model -> load_chat_model

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from src.llm import load_model
+from src.llm import load_chat_model
 
 
 def main():
     # Load the pretrained language model with 4-bit quantization and
     # tokenizer from Hugging Face Hub, wrapped in LangChain (ChatHuggingFace)
-    llm = load_model()
+    llm = load_chat_model()
 
     example = llm.invoke("What is 2+2?")
     print(example)
